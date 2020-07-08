5f413c028f61ecad91e43bd3cd178b2f1436c82e
albertsunn <67825607+albertsunn@users.noreply.github.com>
2020-07-09 01:49:53 +0300

    Update Assignment_1_if_Statements.py

---
+++ Assignment_1_if_Statements.py
@@ -0,0 +1,6 @@
+name = "kerem"
+name1 = input("What is your name? : ")
+if name1 == name :
+    print("Hello, " + name + "! The password is : W@12")
+else :
+    print("Hello, " +name1 + "! See you later.")
